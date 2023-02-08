184afda6e9379df120df602b5ea95001b1c15ed9
pachomoncayo <pachomoncayo@yahoo.com>
2023-02-07 21:52:55 -0500

    Configurando el servidor

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-const port=3000;
+const port=process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server started on http://localhost:${port}`);
 });
